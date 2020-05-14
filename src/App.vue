4be501c9051f40ed4094cb950fe4b1e8c051e84c
Felipe Fontoura <felipefontouramec@gmail.com>
2020-05-14 11:29:18 +0200

    Initialize project using Create Vue App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div  id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br/>
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node</h6>
+    </div>
+    <div id="main"><!--Aqui é aonde vamos criar nossa aplicação-->
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>Prefixos <span class="badge badge-info">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group"><!--Aqui vamos criar listas-->
+                  <li class="list-group-item"></li>
+                </ul> 
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>Sufixos <span class="badge badge-info">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                 
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #F1F1F1;
+  padding-top: 30px;
+  padding-bottom: 30px;  
 }
 </style>
